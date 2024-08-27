5e7113b78031dac13a90dc57bb0ee33fe7e62f22
Jean-François Bustarret <jf@bustarret.com>
2024-08-27 19:38:01 +0200

    📝 README, better examples (#12)

--- inline/inline_example_test.go
+++ inline/inline_example_test.go
@@ -2,8 +2,10 @@ package inline_test
 
 import (
 	"github.com/a-h/templ"
+	"github.com/jfbus/templ-components/icon"
 	"github.com/jfbus/templ-components/inline"
 	"github.com/jfbus/templ-components/input"
+	"github.com/jfbus/templ-components/position"
 	"github.com/jfbus/templ-components/size"
 )
 
@@ -11,16 +13,20 @@ func ExampleC() {
 	_ = inline.C(inline.D{
 		Value:    "Previous value",
 		IconSize: size.S,
+		Focus:    "itemtitle",
 		Edit: input.C(input.D{
-			Name:      "title",
-			Label:     "Title",
-			HideLabel: true,
-			Value:     "Previous value",
-			Size:      size.S,
-			Loader:    true,
+			Name:         "title",
+			Label:        "Title",
+			HideLabel:    true,
+			Value:        "Previous value",
+			Icon:         icon.CornerDownLeft,
+			IconPosition: position.End,
+			Size:         size.S,
+			Loader:       true,
 			Attributes: templ.Attributes{
 				"hx-post":   "/items/update_title/24",
 				"hx-target": "#item_24",
+				"x-ref":     "itemtitle",
 			},
 		}),
 	})
